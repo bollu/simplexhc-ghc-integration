53e0a3b2f256d5533794ab3dcf64b400206605f0
Siddharth Bhat <siddu.druid@gmail.com>
2017-12-22 19:00:26 +0530

    [WIP] actually create a core pass

--- src/Sxhc/Plugin.hs
+++ src/Sxhc/Plugin.hs
@@ -6,7 +6,18 @@ plugin = defaultPlugin {
 	installCoreToDos = install
 }
 
+pass :: ModGuts -> CoreM ModGuts
+pass x = do
+  flags <- getDynFlags
+  mod <- getModule
+  -- coreToStg  flags mod
+  putMsgS "Hello!Hello!"
+  return x
+
+coreToDoFromPass :: CoreToDo
+coreToDoFromPass = CoreDoPluginPass "DumpSxhcStg" pass
+
+
 install :: [CommandLineOption] -> [CoreToDo] -> CoreM [CoreToDo]
 install _ todo = do
-  putMsgS "Hello!"
-  return todo
+  return $ todo ++ [coreToDoFromPass]
