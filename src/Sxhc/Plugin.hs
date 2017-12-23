1bf2e08cc7ed0eb16118724dbb144c1c7f222bd4
Siddharth Bhat <siddu.druid@gmail.com>
2017-12-23 15:27:55 +0530

    [WIP] pretty-print the original STG

--- src/Sxhc/Plugin.hs
+++ src/Sxhc/Plugin.hs
@@ -1,6 +1,7 @@
 module Sxhc.Plugin (plugin) where
 import GhcPlugins
 import CoreToStg
+import CorePrep
 import StgSyn
 import Data.Foldable
 import Data.Traversable
@@ -19,16 +20,21 @@ pass :: ModGuts -> CoreM ModGuts
 pass guts = do
   flags <- getDynFlags
   mod <- getModule
+  env <- getHscEnv
 
   let coreProgram = mg_binds guts
-  let stgProgram = coreToStg flags mod coreProgram
+  let tycons = mg_tcs guts
+  let modloc = ModLocation {
+  	ml_hs_file  = Nothing,
+  	ml_hi_file  = "Example.hi",
+   	ml_obj_file = "Example.o"
+   }
+  preppedProgram <- liftIO $ corePrepPgm env mod modloc coreProgram  tycons
+
+  let stgProgram = coreToStg flags mod preppedProgram
   printStgProgram flags stgProgram
 
-  -- coreToStg flags (mg_module guts) prep 
-  -- coreToStg  flags mod
   putMsgS "Hello!Hello!"
-  -- return x
-  -- bindsOnlyPass (\coreprogram -> coreToStg flags mod coreprogram) guts
   return guts
 
 coreToDoFromPass :: CoreToDo
