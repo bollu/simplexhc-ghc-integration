21d2817744e42fa020c9c5e12a9a2002496a0f9e
Siddharth Bhat <siddu.druid@gmail.com>
2017-12-23 14:57:53 +0530

    [WIP] something runs, we crash though :)

--- src/Sxhc/Plugin.hs
+++ src/Sxhc/Plugin.hs
@@ -1,18 +1,35 @@
 module Sxhc.Plugin (plugin) where
 import GhcPlugins
+import CoreToStg
+import StgSyn
+import Data.Foldable
+import Data.Traversable
 
 plugin :: Plugin
 plugin = defaultPlugin {
 	installCoreToDos = install
 }
 
+printStgProgram :: DynFlags -> [StgTopBinding] -> CoreM ()
+printStgProgram flags binds = forM_ binds (putMsgS . showSDoc flags . ppr) 
+
+-- bindsOnlyPass :: (CoreProgram -> CoreM CoreProgram) -> ModGuts -> CoreM ModGuts
+-- coreToStg :: coreToStg :: DynFlags -> Module -> CoreProgram -> [StgTopBinding] 
 pass :: ModGuts -> CoreM ModGuts
-pass x = do
+pass guts = do
   flags <- getDynFlags
   mod <- getModule
+
+  let coreProgram = mg_binds guts
+  let stgProgram = coreToStg flags mod coreProgram
+  printStgProgram flags stgProgram
+
+  -- coreToStg flags (mg_module guts) prep 
   -- coreToStg  flags mod
   putMsgS "Hello!Hello!"
-  return x
+  -- return x
+  -- bindsOnlyPass (\coreprogram -> coreToStg flags mod coreprogram) guts
+  return guts
 
 coreToDoFromPass :: CoreToDo
 coreToDoFromPass = CoreDoPluginPass "DumpSxhcStg" pass
